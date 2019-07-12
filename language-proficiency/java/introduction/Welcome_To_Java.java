b2f44be5c726df12abf12b635e91bd851b1d55c3
Conor <conormichaelcoleman@gmail.com>
2019-07-12 17:25:49 +0100

    adding more basic java solutions and fixing some formatting on others

--- language-proficiency/java/introduction/Welcome_To_Java.java
+++ language-proficiency/java/introduction/Welcome_To_Java.java
@@ -1,6 +1,7 @@
-public class Welcome_To_Java {
-
-    public static void main(String[] args) {
+public class Welcome_To_Java
+{
+    public static void main(String[] args)
+    {
         System.out.print("Hello, World.\nHello, Java.");
     }
 }
