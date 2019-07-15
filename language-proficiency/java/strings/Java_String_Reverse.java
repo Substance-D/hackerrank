9f25bc5868d3c14a3a158bd0f4915a707fdb51a1
Conor <conormichaelcoleman@gmail.com>
2019-07-15 16:33:42 +0100

    new java strings folder and reverse string solution

---
+++ language-proficiency/java/strings/Java_String_Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Java_String_Reverse
+{
+    public static void main(String args [])
+    {
+        Scanner scan = new Scanner(System.in);
+        String toReverse = scan.nextLine();
+        scan.close();
+        String reversed = "";
+        for(int i = toReverse.length() - 1; i >= 0; i--) reversed += toReverse.charAt(i);
+        if(toReverse.equals(reversed)) System.out.println("Yes");
+        else System.out.println("No");
+
+    }
+}
