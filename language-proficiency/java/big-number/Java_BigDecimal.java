41772cc0afd5c072a3384d223165e263c5e499d8
Conor <conormichaelcoleman@gmail.com>
2019-07-16 17:51:15 +0100

    start of big decimal soln

---
+++ language-proficiency/java/big-number/Java_BigDecimal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+
+public class Java_BigDecimal
+{
+    public static void main(String args [])
+    {
+        Scanner scan = new Scanner(System.in);
+        BigDecimal a = new BigDecimal("0.1");
+        BigDecimal b = new BigDecimal(".1");
+        System.out.println(a.toString() + "\n" + b.toString());
+        System.out.println("");
+    }
+}
