7de87de2f53db99dbc8680387b6e407f6c22ccbd
CoColeman <conormichaelcoleman@gmail.com>
2019-07-16 23:30:38 +0100

    adjust bigdecimal soln, need to account for braindead locked io

--- language-proficiency/java/big-number/Java_BigDecimal.java
+++ language-proficiency/java/big-number/Java_BigDecimal.java
@@ -1,14 +1,31 @@
 import java.util.Scanner;
 import java.math.BigDecimal;
+import java.util.Hashtable;
 
 public class Java_BigDecimal
 {
     public static void main(String args [])
     {
+        Hashtable<BigDecimal, Integer> decimalLengths = new Hashtable<BigDecimal, Integer>();
+
         Scanner scan = new Scanner(System.in);
-        BigDecimal a = new BigDecimal("0.1");
-        BigDecimal b = new BigDecimal(".1");
-        
+
+        String temp;
+
+        temp = scan.nextLine();
+        BigDecimal a = new BigDecimal(temp);
+        decimalLengths.put(a, temp.length());
+        /*
+        temp = scan.nextLine();
+        BigDecimal b = new BigDecimal(temp);
+        decimalLengths.put(b, temp.length());
+        */
+        Integer n = decimalLengths.get(a);
+        Integer nprime = a.toString().length();
+        System.out.println(n + " " + nprime);
+
+
+
 
     }
 }
