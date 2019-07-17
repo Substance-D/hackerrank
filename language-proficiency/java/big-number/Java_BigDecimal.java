d08d5d04a4727f63b8d4d13bce36f16f0df31799
Conor <conormichaelcoleman@gmail.com>
2019-07-17 21:54:23 +0100

    almost complete solution for BigDecimal, missing 0.12 .12 distinction

--- language-proficiency/java/big-number/Java_BigDecimal.java
+++ language-proficiency/java/big-number/Java_BigDecimal.java
@@ -1,31 +1,57 @@
-import java.util.Scanner;
 import java.math.BigDecimal;
-import java.util.Hashtable;
+import java.util.*;
+class Java_BigDecimal{
 
-public class Java_BigDecimal
-{
-    public static void main(String args [])
-    {
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
         Hashtable<BigDecimal, Integer> decimalLengths = new Hashtable<BigDecimal, Integer>();
 
-        Scanner scan = new Scanner(System.in);
+        String[] sprime = new String[n];
+        for(int i = 0; i < sprime.length; i++)
+        {
+            sprime[i] = s[i];
+        }
+        s = sprime;
+        BigDecimal[] decimalValues = new BigDecimal[n];
+        for(int j = 0; j < s.length; j++)
+        {
+            decimalValues[j] = new BigDecimal(s[j]);
+            decimalLengths.put(decimalValues[j], s[j].length());
+        }
+        java.util.Arrays.sort(decimalValues, Collections.reverseOrder());
 
-        String temp;
+        Integer f, r;
+        for(int k = 0; k < decimalValues.length; k++)
+        {
+            f = decimalLengths.get(decimalValues[k]);
+            r = decimalValues[k].toString().length();
+            if(r != f)
+            {
+                if(r > f) s[k] = decimalValues[k].toString().substring(1);
+                else
+                {
+                    s[k] = decimalValues[k].toString();
+                    while(s[k].length() < f) s[k] = "0" + s[k];
+                }
+            }
 
-        temp = scan.nextLine();
-        BigDecimal a = new BigDecimal(temp);
-        decimalLengths.put(a, temp.length());
-        /*
-        temp = scan.nextLine();
-        BigDecimal b = new BigDecimal(temp);
-        decimalLengths.put(b, temp.length());
-        */
-        Integer n = decimalLengths.get(a);
-        Integer nprime = a.toString().length();
-        System.out.println(n + " " + nprime);
+            else s[k] = decimalValues[k].toString();
+        }
 
 
-
-
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
     }
+
 }
