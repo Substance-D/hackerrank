d2762744dd7632e27c81c92ecc67cbfea268d85a
Conor <conormichaelcoleman@gmail.com>
2019-07-24 09:18:05 +0100

    start of hashset soln

---
+++ language-proficiency/java/data-structures/HRHashset.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class HRHashset
+{
+    public static void main(String args [])
+    {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
