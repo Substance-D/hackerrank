c5407a7f7d63bf52c0a0b481f790641ca8eb563d
Conor <conormichaelcoleman@gmail.com>
2019-07-26 15:14:02 +0100

    soln for method overriding

---
+++ language-proficiency/java/object-oriented-programming/HRMethod_Overriding.java
@@ -0,0 +1,52 @@
+//Start of code segment locked by HackerRank
+import java.util.*;
+
+class Sports
+{
+
+    String getName()
+    {
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers()
+    {
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports
+{
+    @Override
+    String getName()
+    {
+        return "Soccer Class";
+    }
+//End of code segment locked by HackerRank
+
+/*
+A method override is as simple as writing the exact same method signature in the child class. In this case literally all
+that needs to be done is copy pasting the method from the Sports class and changing n to 11 in the print statement.
+*/
+    @Override
+    void getNumberOfTeamMembers()
+    {
+        System.out.println("Each team has 11 players in " + getName());
+    }
+//Start of code segment locked by HackerRank
+}
+
+public class HRMethod_Overriding
+{
+
+    public static void main(String []args)
+    {
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
+}
+//End of code segment locked by HackerRank
